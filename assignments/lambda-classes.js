3bc532ed822214720a8ae458d9c5d10ec08942aa
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-07-25 15:33:23 -0400

    First two clsses created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+
+
+// Person 
+
+class Person{
+    constructor(attrs){
+        this.newName = attrs.name,
+        this.newAge = attrs.age,
+        this.newLocation = attrs.location
+    }
+    speak(){
+        console.log(`Hello, my name is ${this.newName} and I am from ${this.newLocation}.`)
+    }
+}
+
+
+class Instructor extends Person{
+    constructor(instruAttrs){
+        super(instruAttrs);
+this.newSpecialty = instruAttrs.specialty,
+this.newFavlanguage = instruAttrs.favLanguage,
+this.newCatchphrase = instruAttrs.catchPhrase;
+    }
+    
+}
